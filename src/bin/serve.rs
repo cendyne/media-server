b31098dbb68d466cf7c64767ffed3c5d9d78e17f
Cendyne <cendyne@cendyne.dev>
2022-04-12 20:01:17 -0500

    Switch to GNU AGPLv3

--- src/bin/serve.rs
+++ src/bin/serve.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2022 Cendyne.
+// This file is part of Cendyne Media-Server.
+
+// Cendyne Media-Server is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// Cendyne Media-Server is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #[macro_use]
 extern crate rocket;
 extern crate diesel;
